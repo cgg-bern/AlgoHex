33030e7d4d491d9ae1072fe392babb6cca8d1126
Martin Heistermann <git@mheistermann.de>
2024-01-16 11:02:48 +0100

    Update GMM finder

--- cmake/FindGMM.cmake
+++ cmake/FindGMM.cmake
@@ -1,61 +1,27 @@
-# - Try to find GMM
-# Once done this will define
-#  GMM_FOUND          - System has GMM
-#  GMM_INCLUDE_DIRS   - The GMM include directories
-#  GMM_COMPILER_FLAGS - Compiler flags that are necessary for GMM
+set(GMM_DIR $ENV{GMM_DIR} CACHE PATH "GMM directory (contain gmm/gmm.h).")
 
-if (GMM_INCLUDE_DIR)
-  # in cache already
-  set(GMM_FOUND TRUE)
-  set(GMM_INCLUDE_DIRS "${GMM_INCLUDE_DIR}" )
-else (GMM_INCLUDE_DIR)
-
-# Check if the base path is set
-if ( NOT CMAKE_WINDOWS_LIBS_DIR )
-  # This is the base directory for windows library search used in the finders we shipp.
-  set(CMAKE_WINDOWS_LIBS_DIR "c:/libs" CACHE STRING "Default Library search dir on windows." )
-endif()
-
-
-find_path( GMM_INCLUDE_DIR 
-           NAMES gmm/gmm.h 
-           PATHS $ENV{GMM_DIR}
-                 /usr/include
-                 /usr/include
+find_path(GMM_INCLUDE_DIR
+           NAMES gmm/gmm.h
+           PATHS ${GMM_DIR}
+                 ${GMM_DIR}/include
                  /usr/local/include
-                 ~/sw/gmm-5.0/include
-                 ~/sw/gmm-4.2/include
-                 ~/sw/gmm-4.1/include
-		 "c:\\libs\\gmm-4.2\\include"
-		 "c:\\libs\\gmm-4.1\\include"
-                 "c:\\libs\\gmm-3.0\\include"
-                 "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-5.0/include"
-                 "${CMAKE_WINDOWS_LIBS_DIR}/general/gmm-4.2/include"
-		 ${PROJECT_SOURCE_DIR}/MacOS/Libs/gmm-3.1/include
-                 ../../External/include
-                 /ACG/acgdev/gcc-4.3-i686/gmm-4.1/include/
-                 /ACG/acgdev/gcc-4.3-i686/gmm-3/include/
-                 ${module_file_path}/../../../External/include
+                 /usr/include
           )
 
-set(GMM_INCLUDE_DIRS "${GMM_INCLUDE_DIR}" )
-
-
-# use c++ headers as default
-IF (WIN32)
-  set(GMM_COMPILE_DEFINITIONS "_SCL_SECURE_NO_DEPRECATE" CACHE STRING "GMM Compiler Definitions")
-#  add_definitions(-D_SCL_SECURE_NO_DEPRECATE)
-ELSE(WIN32)
-  set(GMM_COMPILE_DEFINITIONS "" CACHE STRING "GMM Compiler Definitions")
-ENDIF(WIN32)
-
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LIBCPLEX_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(GMM  DEFAULT_MSG
-                                  GMM_INCLUDE_DIR)
+find_package_handle_standard_args(GMM DEFAULT_MSG GMM_INCLUDE_DIR)
 
-mark_as_advanced(GMM_INCLUDE_DIR GMM_COMPILE_DEFINITIONS)
+if(GMM_FOUND AND NOT TARGET GMM::GMM)
+    add_library(GMM::GMM INTERFACE IMPORTED)
+    target_include_directories(GMM::GMM INTERFACE ${GMM_INCLUDE_DIR})
+    if(MSVC)
+        target_compile_definitions(GMM::GMM INTERFACE "_SCL_SECURE_NO_DEPRECATE")
+    endif()
+    target_compile_options(GMM::GMM INTERFACE
+          $<$<COMPILE_LANG_AND_ID:CXX,AppleClang,Clang>:-Wno-deprecated-declarations>
+          )
+endif()
 
-endif(GMM_INCLUDE_DIR)
+mark_as_advanced(GMM_INCLUDE_DIR)
+
 
