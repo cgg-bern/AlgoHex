c5a2f69bfea7c8b67ebc3629da793cf9eb90660a
Martin Heistermann <git@mheistermann.de>
2025-03-10 12:05:14 +0100

    CMake/deps: Update CLI11

--- cmake/AlgoHexDependencies.cmake
+++ cmake/AlgoHexDependencies.cmake
@@ -91,7 +91,7 @@ endif()
 if(NOT TARGET CLI11::CLI11)
     FetchContent_Declare(cli11
         GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
-        GIT_TAG        v2.3.2
+        GIT_TAG        v2.5.0
         SOURCE_DIR "${EXTERNAL_DIR}/CLI11"
         )
     FetchContent_MakeAvailable(cli11)
